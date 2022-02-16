a144bd76374d962cfea6c38793f18f60d48f7acb
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-02-16 12:01:26 -0500

    created project and installed material ui

---
+++ tinder/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+     <h1>Lets build the mern Tinder</h1>
+
+     <Header />
+     {/* TinderCards */}
+     {/* SwipeButtons*/}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+import PersonIcon from '@mui/icons-material/Person';
+
+function Header() {
+  return (
+    <div className= 'header'>
+<h2>I am a header</h2>
+<PersonIcon />
+
+    </div>
+  )
+}
+
+export default Header
