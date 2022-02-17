5a4661c7f19550b8402e1233ebb6d3008de05dd3
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-02-17 12:10:34 -0500

    connected the sevrer to the mongo db

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,15 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cors from 'cors';
+import Cards from "./dbCards.js";
 
 // App Config
 const app = express();
-const port = process.env.PORT || 8001
-// MiddleWares
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://admin:kEk5a1lFJE8YaImc@cluster0.zngat.mongodb.net/tinderdb?retryWrites=true&w=majority`;
+
+
+//Middle ware
+app.use(express.json())
+app.use(Cors());
 
 //DB config
-
+mongoose.connect(connection_url, {
+ 
+});
 //API End Points
-app.get('/', (req, res) =>  res.status(200).send('Hello World'))
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 // Listener
 
-app.listen(port, ()  => console.log(`listening on localhost:${port}`));
+app.listen(port, () => console.log(`listening on localhost:${port}`));
